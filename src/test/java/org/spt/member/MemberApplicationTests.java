8e61ee9706f4cd61e5f76a6af3c13844324f171d
cbot918 <cbot918@gmail.com>
2024-04-14 20:30:05 +0800

    init project

---
+++ src/test/java/org/spt/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spt.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
